23d17826c80f3b33731d8e061a863f6dcde3619b
JaceQ <JaceQ@Kompiuter>
2016-10-06 11:51:58 +0200

    Change-Id: Ic2a18b65bc132dc1a3220bc2257dd2187bc571af

--- PAI_CW2/src/WebsiteApp.java
+++ PAI_CW2/src/WebsiteApp.java
@@ -5,6 +5,7 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -22,7 +23,7 @@ public class WebsiteApp {
 		}
 
 		
-		openWebsite("http://www.x-kom.pl/kontakt/");
+		openWebsite("http://www.google.pl/");
 	}
 	
 	public static void openWebsite(String path){
@@ -30,14 +31,12 @@ public class WebsiteApp {
 		    URL myURL = new URL(path);
 		    URLConnection myURLConnection = myURL.openConnection();
 		    myURLConnection.connect();
-		    //System.out.println("Gotowe!");
 	        BufferedReader in = new BufferedReader(new InputStreamReader(myURL.openStream()));
 
 	        String inputLine;
 	        while ((inputLine = in.readLine()) != null){
 	        	//System.out.println(inputLine);
 	        	findEmails(inputLine);
-	        	
 	        }
 
 	        in.close();
@@ -45,27 +44,19 @@ public class WebsiteApp {
 		catch (MalformedURLException e) { 
 		    // new URL() failed
 		    // ...
+			System.out.println("Problem z polaczeniem: " + e);
 		} 
 		catch (IOException e) {   
 		    // openConnection() failed
 		    // ...
+			System.out.println("Problem z polaczeniem: " + e);
 		}
 	}
 	
 	private static void findEmails(String text){
 		String emailPattern1 = ("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$");
 		String emailPattern2 = ("(@)?(href=')?(HREF=')?(HREF=\")?(href=\")?(mailto:)?[a-zA-Z_0-9\\-]+(\\.\\w[a-zA-Z_0-9\\-]+)+(/[#&\\n\\-=?\\+\\%/\\.\\w]+)\"?");
-		String httpPattern = ("(@)?(href=')?(HREF=')?(HREF=\")?(href=\")?(http://)?[a-zA-Z_0-9\\-]+(\\.\\w[a-zA-Z_0-9\\-]+)+(/[#&\\n\\-=?\\+\\%/\\.\\w]+)\"?");
-		//String httpPattern = ("^(https?|ftp|file|http)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]");
-		
-		/*Pattern p = Pattern.compile("<!DOCTYPE html>");
-		Matcher m = p.matcher(text.toLowerCase()); 
-		
-		while(m.find()) {                      
-			System.out.println(text.substring(m.start(), m.end()).toLowerCase());      
-		}*/
-		
-		//String text = "I will come and meet you at the woods 123woods and all the woods";
+		String httpPattern = ("(@)?(href=')?(HREF=')?(HREF=\")?(href=\")?(http://)?+(\\.\\w[a-zA-Z_0-9\\-]+)+(/[#&\\n\\-=?\\+\\%/\\.\\w]+)\"?");
 
 		List<String> tokens = new ArrayList<String>();
 		tokens.add(httpPattern);
@@ -75,12 +66,13 @@ public class WebsiteApp {
 		String patternString = "\\b(" + StringUtils.join(tokens, "|") + ")\\b";
 		Pattern pattern = Pattern.compile(patternString);
 		Matcher matcher = pattern.matcher(text);
-
+		 
 		while (matcher.find()) {
-			System.out.println(matcher.group(0));
-			System.out.println(matcher.group(1));
+			//allMatches.add(matcher.group());
+			System.out.println(matcher.group());
 		}
-	
+		
+		
 	}
 		
 	
