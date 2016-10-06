419d1332daaf3ba415a412a7a61fc705d1a7dd36
Jacek <Jacek@JaceQ>
2016-10-06 10:01:49 +0200

    First change

---
+++ TimeBasedCurrency/src/Server/ServerConstants.java
@@ -0,0 +1,14 @@
+package Server;
+
+public final class ServerConstants {
+	
+	public ServerConstants() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public final static int CLOSE = 0;
+	public final static int SUBMIT_NEW_SERVICE = 1;
+	public final static int WITHDRAW_YOUR_SERVICE = 2;
+	public final static int SHOW_ALL_SERVICES = 3;
+	public final static int RESERVE_SERVICE = 4;
+}
---
+++ TimeBasedCurrency/src/Server/ServiceConstants.java
@@ -0,0 +1,15 @@
+package Server;
+
+public final class ServiceConstants {
+	
+	public ServiceConstants() {
+	
+	}
+	
+	public final static int NO_INFO = 0;
+	public final static int NEW_SERVICE = 1;
+	public final static int RESERVED_SERVICE = 2;
+	public final static int WITHDRAWN_SERVICE = 3;
+	
+	public final static String EMPTY = "";
+}
