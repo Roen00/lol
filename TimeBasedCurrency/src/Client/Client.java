cfa990221cfdb5e3f6377ca5f6b761413a0218b9
TheJaceQ <jacek.jk.kulis@gmail.com>
2016-11-17 18:02:53 +0100

    Added few classes

--- TimeBasedCurrency/src/Client/Client.java
+++ TimeBasedCurrency/src/Client/Client.java
@@ -7,30 +7,103 @@ import java.net.Socket;
 import java.io.PrintWriter;
  
 public class Client{
- 
+	private int port;
+	private String hostName;
+	
+	//Constructors
+	public Client(){	
+	}
+	
+	public Client(int port){
+		this.port = port;
+		this.hostName = "localhost";
+	}
+	
+	public Client(String hostName){
+		this.port = 3000;
+		this.hostName = hostName;
+	}
+	
+	public Client(int port, String hostName){
+		this.port = port;
+		this.hostName = hostName;
+	}
+	
 	public static void main(String[] args) throws IOException{
-		String hostName = "localhost";
-		Socket echoSocket = new Socket(hostName, 9890);
+		Client client = new Client(3000, "localhost");
+		Socket clientSocket = new Socket(client.hostName, client.port);
 		
-		PrintWriter out = new PrintWriter(echoSocket.getOutputStream(), true);
-		BufferedReader in = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
+		
+		PrintWriter clientOutputStream = new PrintWriter(clientSocket.getOutputStream(), true);
+		BufferedReader clientInputStream = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 		BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
 		
-		System.out.println("Polaczono z serverem: " + hostName);
-	
-		 String userInput;
-	     while ((userInput = stdIn.readLine()) != null) {
-	         out.println(userInput);
-	         System.out.println("echo: " + in.readLine());
-	     }
+		
+		//Sending name of client
+		while(true){
+			String line = clientInputStream.readLine();
+			if (line.startsWith("Podaj")){
+				clientOutputStream.println(client.getClientName());
+			}
+			else if (line.toLowerCase().startsWith("error")){
+				System.out.println("Blad, jeszcze raz");
+			}
+			else if (line.toLowerCase().startsWith("accepted")){
+				System.out.println("Zaakceptowano");
+				break;
+			}
+		}
+		
+		
+		//Decision from client
+		while(true){
+			//Sending information to server that connection is ready
+			clientOutputStream.println("ready");
+			for (int i = 0; i < 6; i++){
+				System.out.println(clientInputStream.readLine());
+			}
+			String userDecision = stdIn.readLine();
+			clientOutputStream.println(userDecision);
+			break;
+		}
+		
+		
+//		while ((userInput = stdIn.readLine()) != null) {
+//			out.println(userInput);
+//			System.out.println("Server: " + in.readLine());
+//		}
 			
-	     echoSocket.close();
+		clientSocket.close();
 	
-	     try {
-	    	 Thread.sleep(1000);
-	     } catch (InterruptedException e) {
-	    	 // TODO Auto-generated catch block
-	    	 e.printStackTrace();
-	     }
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
+	
+	
+	public void setPort(int port){
+		this.port = port;
+	}
+	
+	public int getPort(){
+		return this.port;
+	}
+	
+	public void setHostName(String hostName){
+		this.hostName = hostName;
+	}
+	
+	public String getHostName(){
+		return this.hostName;
+	}
+	
+	public String getClientName() throws IOException{
+		BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Podaj nazwe uzytkownika: ");
+		return stdIn.readLine();
+	}
+	
 }
--- TimeBasedCurrency/src/Server/Server.java
+++ TimeBasedCurrency/src/Server/Server.java
@@ -6,48 +6,140 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
  
 public class Server {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Uruchomiono serwer.");
-        ServerSocket serverSocket = new ServerSocket(9890);
-        
-        try {
-            while (true) {
-            	Connection connection = new Connection(serverSocket.accept());
-            	connection.start();
-            }
-        } 
+	private final int port = 3000;
+	//private int numberOfClientsConnected = 0;
+	
+	private Map<String, List<Service>> data = new LinkedHashMap<String, List<Service>>();
+	
+	public static void main(String[] args) throws Exception {
+		Server server = new Server();
+		ServerSocket serverSocket = new ServerSocket(server.port);
+		
+		try {
+			while (true) {
+				Connection connection = server.new Connection(serverSocket.accept());
+				connection.start();
+			}
+		} 
 		catch(Exception e){
 			System.out.println(e.getMessage());
-        }
-        serverSocket.close();
-    }
- 
-    static class Connection extends Thread {
-        private Socket socket;
-        
-        public Connection(Socket socket) {
-        	System.out.println("Klient polaczyl sie z serverem.");
-            this.socket = socket;
-        }
- 
-        public void run() {
-            try {
-            	PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-            	BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-
-  
-                String inputLine;
-                while ((inputLine = in.readLine()) != null) {
-                   out.println(inputLine);
-                }
-                
-                socket.close();
-            } 
+		}
+		serverSocket.close();
+	}
+	
+	public Server(){
+		System.out.println("Uruchomiono serwer.");
+	}
+	
+	
+	
+	private class Connection extends Thread {
+		private Socket socket;
+		private PrintWriter serverOutputStream;
+		private BufferedReader serverInputStream;
+		private int decision;
+		private String nameOfClient;
+		
+		public Connection(Socket socket) {
+			this.socket = socket;
+		}
+		
+		public void run() {
+			try {
+				System.out.println("Klient polaczyl sie z serverem.");
+				
+				serverOutputStream = new PrintWriter(socket.getOutputStream(), true);
+				serverInputStream = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				//String inputLine;
+				
+				
+				//Getting name from user
+				while(true){
+					if(getNameFromClient(serverOutputStream, serverInputStream, nameOfClient)){
+						break;
+					}
+				}
+				
+				//Sending to client information about possible options
+				while (true){
+					if((serverInputStream.readLine().toLowerCase().startsWith("ready"))) { 
+						showOptions(serverOutputStream);
+						break;
+					}
+				}
+				
+				
+				while(true){
+					decision = Integer.parseInt(serverInputStream.readLine());
+					
+					if(decision == ServerConstants.CLOSE){
+						System.out.println("Client wybral zamkniecie polaczenia");
+						//out.println("Zamykam polaczenie!");
+						return;
+					}
+					else if (decision == ServerConstants.SUBMIT_NEW_SERVICE){
+						System.out.println("Client wybral zgloszneie uslugi");
+						break;
+					}
+					else if (decision == ServerConstants.WITHDRAW_YOUR_SERVICE){
+						System.out.println("Client wybral zamkniecie uslugi");
+						break;
+					}
+					else if (decision == ServerConstants.SHOW_ALL_SERVICES){
+						System.out.println("Client wybral wyswietlenie wszystkich uslug");
+						break;
+					}
+					else if (decision == ServerConstants.RESERVE_SERVICE){
+						System.out.println("Client wybral rezerwacje uslugi");
+						break;
+					}
+					
+					System.out.println("Przyjalem decyzje i robie cos");
+					
+				}
+				
+				
+				
+				
+				socket.close();
+			}
 			catch (IOException e) {
 				System.out.println(e.getMessage());
-            } 
-        }
-    }
+			}
+		}
+}
+	
+	private void showOptions(PrintWriter serverOutputStream){
+		serverOutputStream.println(	"Co chcesz zrobic? \n" +
+					"0. Wyjdz i zamknij po³¹czenie \n" +
+					"1. Zg³oœ now¹ us³ugê. \n" + 
+					"2. Wycofaj swoj¹ us³ugê. \n" + 
+					"3. Wyœwietl wszystkie dostêpne us³ugi. \n" +
+					"4. Zarezerwuj us³ugê. \n");
+	}
+	
+	private boolean getNameFromClient(PrintWriter serverOutputStream, BufferedReader serverInputStream, String nameOfClient) throws IOException{
+		serverOutputStream.println("Podaj nazwe uzytkownika");
+		nameOfClient = serverInputStream.readLine();
+		if (nameOfClient == null){
+			return false;
+		}
+		synchronized(data){
+			if (!data.containsKey(nameOfClient)){
+				data.put(nameOfClient, new LinkedList<>());
+				serverOutputStream.println("accepted");
+				return true;
+			}
+			else {
+				serverOutputStream.println("error.");
+			}
+		}
+		return false;
+	}
 }
