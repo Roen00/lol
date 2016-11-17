f7194d92cb5e0f13ef141f43ff7b813474cded1e
TheJaceQ <jacek.jk.kulis@gmail.com>
2016-11-17 15:10:56 +0100

    New classes: service + serviceconstants

--- TimeBasedCurrency/src/Server/Service.java
+++ TimeBasedCurrency/src/Server/Service.java
@@ -1,5 +1,42 @@
 package Server;
 
 public class Service {
-
+	
+	private String nameOfService;
+	private String termOfService;
+	private int statusOfService;
+	
+	
+	public void setNameOfService(String nameOfService){
+		this.nameOfService = nameOfService;
+	}
+	
+	public String getNameOfService(){
+		return this.nameOfService;
+	}
+	
+	public void setTermOfService(String termOfService){
+		this.termOfService = termOfService;
+	}
+	
+	public String getTermOfService(){
+		return this.termOfService;
+	}
+	
+	public void setStatusOfService(int status){
+		this.statusOfService = status;
+	}
+	
+	public int getStatusOfService(){
+		return this.statusOfService;
+	}
+	
+	public Service(){
+		this.nameOfService = ServiceConstants.EMPTY;
+		this.termOfService = ServiceConstants.EMPTY;
+		this.statusOfService = ServiceConstants.NO_INFO;
+	}
+	
+	
+	
 }
--- TimeBasedCurrency/src/Server/ServiceConstants.java
+++ TimeBasedCurrency/src/Server/ServiceConstants.java
@@ -1,5 +1,15 @@
 package Server;
 
-public class ServiceConstants {
-
+public final class ServiceConstants {
+	
+	public ServiceConstants() {
+	
+	}
+	
+	public final static int NO_INFO = 0;
+	public final static int NEW_SERVICE = 1;
+	public final static int RESERVED_SERVICE = 2;
+	public final static int WITHDRAWN_SERVICE = 3;
+	
+	public final static String EMPTY = "";
 }
